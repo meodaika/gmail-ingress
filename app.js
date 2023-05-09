9de1a7bda9be988f0738929e9cbcba5b7609f699
catdobyte <catdobyte@gmail.com>
2023-05-09 16:36:34 +0700

    jfksld

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 3001
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/", (req, res) => res.type("html").json({ load: true }))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 const html = `
 <!DOCTYPE html>
